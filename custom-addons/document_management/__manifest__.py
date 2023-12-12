916e2c859d2fdc1ad776d46fb42c25db46f8d173
lukas <lukasurna@gmail.com>
2023-12-12 11:59:07 +0200

    Documents view

--- custom-addons/document_management/__manifest__.py
+++ custom-addons/document_management/__manifest__.py
@@ -3,8 +3,12 @@
     "version": "1.0",
     "author": "lukasSurna",
     "category": "",
+    "depends": ['base', 'hr'],
     "data": [
         "security/ir.model.access.csv",
+        "views/documents_view.xml",
+        "views/menu_items.xml",
+
     ],
     "installable": True,
     "application": True,
